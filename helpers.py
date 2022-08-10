310fcab704e882de121579d9d5d7d6fdead080e2
agriffin0 <71677738+agriffin0@users.noreply.github.com>
2022-08-10 14:37:26 -0400

    Add files via upload
    initial commit

---
+++ helpers.py
@@ -0,0 +1,55 @@
+import os
+import requests
+import urllib.parse
+import random
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+# From CS50
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+# Adapted from CS50
+def vinyl(album, artist):
+    """Render image of vinyl record with title."""
+    def escape(s):
+        """
+        Escape special characters.
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("vinyl.html", top=escape(album), bottom=escape(artist))
+
+# From CS50
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+def choose_id(count):
+    """Choose random album"""
+    # return random number between 0 and length of table
+    return random.randrange(count)
